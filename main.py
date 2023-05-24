e4d43bc85f8745244457590b5f130079c7b086af
Daniel Kean <124378867+danielkean5@users.noreply.github.com>
2023-05-24 14:16:07 +0000

    commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-
+def fnc():
+    while True:
+        Num = input("Input Number:\n")
+        if Num.isnumeric():
+            if int(Num) > 0:
+                return Num
+number = int(fnc())
+deno = -1
+ans = 0
+for n in range(number):
+    deno = deno + 2
+    if n % 2 == 0:
+        ans = ans + 4/deno
+    else:
+        ans = ans - 4/deno
+print(ans)
+fnc()
